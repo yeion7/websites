564789f1a938563280099e87e833f306cf7fb07d
yeion7 <jahir045@gmail.com>
2016-04-07 08:35:11 -0500

    day 3

---
+++ day3/app.js
@@ -0,0 +1,15 @@
+function getHex() {
+  return Math.round(Math.random()*16).toString(16);
+}
+
+function getBackgroundRandom() {
+  return `#${getHex()}${getHex()}${getHex()}${getHex()}${getHex()}${getHex()}`
+}
+
+
+const buttonChange = document.getElementById('button-background');
+
+
+buttonChange.addEventListener('click', function() {
+  document.body.style.background = getBackgroundRandom();
+})
