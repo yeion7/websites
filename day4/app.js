ad476131d7b0ba53625f6443fcfaf1c63bca0b7a
yeion7 <jahir045@gmail.com>
2016-04-08 10:45:40 -0500

    day 4

---
+++ day4/app.js
@@ -0,0 +1,16 @@
+let currentColor = 'white';
+
+const palete = document.querySelectorAll('.palete > div');
+const boxes = document.querySelectorAll('.cell');
+
+Array.prototype.forEach.call(palete, function(color) {
+  color.addEventListener('click', function() {
+    currentColor = this.style.background
+  })
+ });
+
+ Array.prototype.forEach.call(boxes, function(box) {
+   box.addEventListener('click', function() {
+     this.style.background = currentColor
+   })
+  });
