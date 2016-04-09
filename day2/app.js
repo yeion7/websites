901f8af277fcc2f5f4eb45f0d661a9e981d21128
yeion7 <jahir045@gmail.com>
2016-04-09 08:42:53 -0500

    added day5

--- day2/app.js
+++ day2/app.js
@@ -1,11 +1,11 @@
 function numeroAleatorio(limite) {
-  return Math.round(Math.random()*limite)
+  return Math.round(Math.random() * limite);
 }
 
 function addRoute(item, link) {
-  item.addEventListener('click', function() {
+  item.addEventListener('click', function () {
     window.location.href = link;
-  })
+  });
 }
 
 const indexPuertaMala = numeroAleatorio(2);
@@ -15,10 +15,10 @@ const linkBueno = 'https://www.youtube.com/watch?v=fzzjgBAaWZw';
 
 const doors = document.querySelectorAll('.icon-door');
 
-Array.prototype.map.call(doors, function(door, index) {
-  if(index==indexPuertaMala) {
+Array.prototype.map.call(doors, function (door, index) {
+  if (index == indexPuertaMala) {
     addRoute(door, linkMalo);
   } else {
     addRoute(door, linkBueno);
   }
-})
+});
--- day3/app.js
+++ day3/app.js
@@ -1,15 +1,13 @@
 function getHex() {
-  return Math.round(Math.random()*16).toString(16);
+  return Math.round(Math.random() * 16).toString(16);
 }
 
 function getBackgroundRandom() {
-  return `#${getHex()}${getHex()}${getHex()}${getHex()}${getHex()}${getHex()}`
+  return `#${getHex()}${getHex()}${getHex()}${getHex()}${getHex()}${getHex()}`;
 }
-
 
 const buttonChange = document.getElementById('button-background');
 
-
-buttonChange.addEventListener('click', function() {
+buttonChange.addEventListener('click', function () {
   document.body.style.background = getBackgroundRandom();
-})
+});
