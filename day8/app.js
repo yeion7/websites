6323de02481e492b9626851b8ed24ecd4af4765b
yeion7 <jahir045@gmail.com>
2016-04-12 07:29:38 -0500

    day 8 resolved

--- day8/app.js
+++ day8/app.js
@@ -1,1 +1,9 @@
+const lamp = document.querySelector('.icon');
 
+let currentShadow = 0;
+
+lamp.addEventListener('click', function () {
+  this.style.color = '#EDDE45';
+  this.style.boxShadow = `0px 0px 30px ${currentShadow}px`;
+  currentShadow += 10;
+});
