e1c5a0ba4bf1f4668a48163d27f52144fa3a954e
yeion7 <jahir045@gmail.com>
2016-04-20 10:47:39 -0500

    day 16 resolved

--- day16/app.js
+++ day16/app.js
@@ -1,7 +1,64 @@
-function getHex() {
-  return Math.round(Math.random() * 16).toString(16);
-}
+document.addEventListener('DOMContentLoaded', function (event) {
+  var container = document.querySelector('.board');
 
-function getBackgroundRandom() {
-  return `#${getHex()}${getHex()}${getHex()}${getHex()}${getHex()}${getHex()}`;
-}
+  makeSpots(20);
+
+  var mouseX = 0;
+  var mouseY = 0;
+
+  container.addEventListener('mousemove', function (e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+  });
+
+  document.addEventListener('touchmove', function (e) {
+    e.preventDefault();
+
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+  }, false);
+
+  for (var i = 0; i < 20; i++) {
+    moveDiv('#spot' + i, randomInt(8, 50));
+  }
+
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  function randomColor() {
+    return '#' + Math.random().toString(16).slice(2, 8);
+  };
+
+  function makeSpots(num) {
+    var spots = [];
+    for (var i = 0; i < num; i++) {
+      spots[i] = '<div class="circle" id="spot' + i  + '"></div>';
+    }
+
+    container.innerHTML = spots.join('\n');
+
+    for (var i = 0; i < num; i++) {
+      var size = randomInt(3, 65);
+      var color = randomColor();
+      let spot = document.querySelector('#spot' + i);
+      spot.style.background = color;
+      spot.style.width = size + 'px';
+      spot.style.height = size + 'px';
+    }
+  };
+
+  function moveDiv(elm, speed) {
+    var xp = 0;
+    var yp = 0;
+    var loop = setInterval(function () {
+      xp += (mouseX - xp) / speed;
+      yp += (mouseY - yp) / speed;
+
+      let spo = document.querySelector(elm);
+      spo.style.left = xp + 'px';
+      spo.style.top = yp + 'px';
+
+    }, 30);
+  };
+});
